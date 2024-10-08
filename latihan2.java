098cc46f23d4388da48cad85285ab124a72fa055
Fazanaufalqinthara <fazanaufalq@gmail.com>
2024-10-08 19:24:35 +0700

    Add files via upload

---
+++ latihan2.java
@@ -0,0 +1,34 @@
+package Test_Koneksi;
+
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author MyBook Hype
+ */
+public class latihan2 {
+static final String DB_Url = "jdbc:mysql://localhost:3306/db_unidha";
+
+
+private static Connection Test_Konek;
+public static Connection DB_Konek() throws SQLException,
+ClassNotFoundException {
+
+try {
+Driver myDriver = new com.mysql.jdbc.Driver();
+DriverManager.registerDriver(myDriver);
+System.out.println("Proses Deteksi Driver Berhasil");
+
+final java.sql.Connection tersambung =
+DriverManager.getConnection(DB_Url, "root", "");
+
+System.out.println("Koneksi Database Berhasil");
+} catch (final SQLException ex) {
+System.out.println("Koneksi Database Gagal");
+}
+return Test_Konek;
+}
+}
